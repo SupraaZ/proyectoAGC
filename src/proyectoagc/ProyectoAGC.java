11396b13037042a95ca0849cb7366a8bae4d3b3a
SupraaZ <adriangran17@gmail.com>
2019-11-28 08:10:13 +0100

    añadidos comentarios

--- src/proyectoagc/ProyectoAGC.java
+++ src/proyectoagc/ProyectoAGC.java
@@ -7,22 +7,23 @@ package proyectoagc;
 
 /**
  *
- * @author Adrian
+ * @author Adrian Granero
+ * Version jrk 8.0
  */
 public class ProyectoAGC {
 
-    public static String veredicto(String valor1, String valor2) {
+    private static String veredicto(String valor1, String valor2) {
        String nota1=valor1;
        String nota2=valor2;
-        if (nota1.equals(nota2)) {
+        if (nota1.equals(nota2)) { /** Comprueba que la nota 1 y la nota 2 sean iguales y muestra un texto si así es */
          return "Perfecto. Has tenido buen criterio";
 }
         
-    if (Integer.valueOf(nota1) < Integer.valueOf(nota2)){
+    if (Integer.valueOf(nota1) < Integer.valueOf(nota2)){ /** Comprueba que la nota esperada por el alumno, sea menor que la real */
         return "Has mejorado, o te ha llegado el éxito inesperado.";
 }
 
-return "Te has confiado. Falta realismo.";
+return "Te has confiado. Falta realismo."; /** Si no es ninguna de ellas, muestra el siguiente texto*/
     }
     /**
      * @param args the command line arguments
